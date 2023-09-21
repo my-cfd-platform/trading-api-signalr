97bead7f5f47860b591f767b4d10ce34ec8504a3
RsSvc <wonderhays@gmail.com>
2023-09-21 11:51:14 +0300

    bidask optimization

--- src/bg/bid_ask_subscriber.rs
+++ src/bg/bid_ask_subscriber.rs
@@ -26,9 +26,13 @@ impl SubscriberCallback<BidAskSbModel> for PricesListener {
         &self,
         messages_reader: &mut MessagesReader<BidAskSbModel>,
     ) -> Result<(), MySbSubscriberHandleError> {
-        while let Some(messages) = messages_reader.get_next_message() {
+        while let Some(messages) = messages_reader.get_all() {
+            let messages = messages
+                .into_iter()
+                .map(|x| x.get_message().to_owned())
+                .collect::<Vec<BidAskSbModel>>();
             let mut write = self.app.bid_ask_aggregator.write().await;
-            write.update(messages.get_message());
+            write.update_vec(messages);
         }
 
         return Ok(());
